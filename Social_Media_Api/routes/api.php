e56bd3e4dd51e34eda5d4d6f3df310cdef9212fe
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2021-09-24 18:37:10 +0200

    Add middle ware
    Update middle ware

--- Social_Media_Api/routes/api.php
+++ Social_Media_Api/routes/api.php
@@ -26,86 +26,91 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['middleware'=>'auth:api'],function()
+Route::group(['middleware'=>'json.response'],function()
 {
-    //Comments
-    Route::get('Comments',[CommentController::class,'index']);
-    Route::get('Comments/{comment}',[CommentController::class,'show']);
-    Route::put('Comments/{comment}',[CommentController::class,'Update']);
-    Route::delete('Comments/{comment}', [CommentController::class,'Delete']);
-    Route::post('Comments', [CommentController::class,'create'])->middleware('verified');;
-    // Posts
-    Route::get('posts',[PostController::class,'index']);
-    Route::get('posts/{post}',[PostController::class,'show']);
-    Route::put('posts/{post}',[PostController::class,'Update']);
-    Route::delete('posts/{post}', [PostController::class,'Delete']);
-    Route::post('posts', [PostController::class,'create'])->middleware('verified');
-    Route::get('posts/Myposts',[UserController::class,'Getposts']);
-    //Replies
-    Route::get('Reply',[ReplyController::class,'index']);
-    Route::get('Reply/{reply}',[ReplyController::class,'show']);
-    Route::put('Reply/{reply}',[ReplyController::class,'Update']);
-    Route::delete('Reply/{reply}', [ReplyController::class,'Delete']);
-    Route::post('Reply', [ReplyController::class,'create'])->middleware('verified');
-    // Reactions on comements
-    Route::get('Reaction',[ReactionController::class,'index']);
-    Route::get('Reaction/{reaction}',[ReactionController::class,'show']);
-    Route::put('Reaction/{reaction}',[ReactionController::class,'Update']);
-    Route::delete('Reaction/{reaction}', [ReactionController::class,'Delete']);
-    Route::post('Reaction', [ReactionController::class,'create'])->middleware('verified');;
-    // Reactions on posts
-    Route::get('Postreaction',[PostreactionController::class,'index']);
-    Route::get('Postreaction/{postreaction}',[PostreactionController::class,'show']);
-    Route::put('Postreaction/{postreaction}',[PostreactionController::class,'Update']);
-    Route::delete('Postreaction/{postreaction}', [PostreactionController::class,'Delete']);
-    Route::post('Postreaction', [PostreactionController::class,'create'])->middleware('verified');;
-    // Reactions on replies
-    Route::get('Replyreaction',[ReplyreactionController::class,'index']);
-    Route::get('Replyreaction/{replyreaction}',[ReplyreactionController::class,'show']);
-    Route::put('Replyreaction/{replyreaction}',[ReplyreactionController::class,'Update']);
-    Route::delete('Replyreaction/{replyreaction}', [ReplyreactionController::class,'Delete']);
-    Route::post('Replyreaction', [ReplyreactionController::class,'create'])->middleware('verified');
-    //Follower
-    Route::get('Follower',[FollowerController::class,'index']);
-    Route::delete('Follower/{follower}', [FollowerController::class,'Delete']);
-    //Following
-    Route::get('Following',[FollowingController::class,'index']);
-    Route::delete('Following/{following}', [FollowingController::class,'Delete']);
-    //FollowRequest
-    Route::get('FollowRequest',[FollowrequestController::class,'index']);
-    Route::put('FollowRequest/{FollowRequest}',[FollowrequestController::class,'Update'])->middleware('verified');
-    Route::delete('FollowRequest/{FollowRequest}', [FollowrequestController::class,'Delete']);
-    Route::post('FollowRequest/{FollowRequest}', [FollowrequestController::class,'create'])->middleware('verified');
-    // Followers
-    Route::get('Follower',[FollowerController::class,'index']);
-    Route::delete('Follower/{follower}', [FollowerController::class,'Delete']);
-    // Followings
-    Route::get('Following',[FollowingController::class,'index']);
-    Route::delete('Following/{following}', [FollowingController::class,'Delete']);
+
+
+    Route::group(['middleware'=>'auth:api'],function()
+    {
+        //Comments
+        Route::get('Comments',[CommentController::class,'index']);
+        Route::get('Comments/{comment}',[CommentController::class,'show']);
+        Route::put('Comments/{comment}',[CommentController::class,'Update']);
+        Route::delete('Comments/{comment}', [CommentController::class,'Delete']);
+        Route::post('Comments', [CommentController::class,'create'])->middleware('verified');;
+        // Posts
+        Route::get('posts',[PostController::class,'index']);
+        Route::get('posts/{post}',[PostController::class,'show']);
+        Route::put('posts/{post}',[PostController::class,'Update']);
+        Route::delete('posts/{post}', [PostController::class,'Delete']);
+        Route::post('posts', [PostController::class,'create'])->middleware('verified');
+        Route::get('posts/Myposts',[UserController::class,'Getposts']);
+        //Replies
+        Route::get('Reply',[ReplyController::class,'index']);
+        Route::get('Reply/{reply}',[ReplyController::class,'show']);
+        Route::put('Reply/{reply}',[ReplyController::class,'Update']);
+        Route::delete('Reply/{reply}', [ReplyController::class,'Delete']);
+        Route::post('Reply', [ReplyController::class,'create'])->middleware('verified');
+        // Reactions on comements
+        Route::get('Reaction',[ReactionController::class,'index']);
+        Route::get('Reaction/{reaction}',[ReactionController::class,'show']);
+        Route::put('Reaction/{reaction}',[ReactionController::class,'Update']);
+        Route::delete('Reaction/{reaction}', [ReactionController::class,'Delete']);
+        Route::post('Reaction', [ReactionController::class,'create'])->middleware('verified');;
+        // Reactions on posts
+        Route::get('Postreaction',[PostreactionController::class,'index']);
+        Route::get('Postreaction/{postreaction}',[PostreactionController::class,'show']);
+        Route::put('Postreaction/{postreaction}',[PostreactionController::class,'Update']);
+        Route::delete('Postreaction/{postreaction}', [PostreactionController::class,'Delete']);
+        Route::post('Postreaction', [PostreactionController::class,'create'])->middleware('verified');;
+        // Reactions on replies
+        Route::get('Replyreaction',[ReplyreactionController::class,'index']);
+        Route::get('Replyreaction/{replyreaction}',[ReplyreactionController::class,'show']);
+        Route::put('Replyreaction/{replyreaction}',[ReplyreactionController::class,'Update']);
+        Route::delete('Replyreaction/{replyreaction}', [ReplyreactionController::class,'Delete']);
+        Route::post('Replyreaction', [ReplyreactionController::class,'create'])->middleware('verified');
+        //Follower
+        Route::get('Follower',[FollowerController::class,'index']);
+        Route::delete('Follower/{follower}', [FollowerController::class,'Delete']);
+        //Following
+        Route::get('Following',[FollowingController::class,'index']);
+        Route::delete('Following/{following}', [FollowingController::class,'Delete']);
+        //FollowRequest
+        Route::get('FollowRequest',[FollowrequestController::class,'index']);
+        Route::put('FollowRequest/{FollowRequest}',[FollowrequestController::class,'Update'])->middleware('verified');
+        Route::delete('FollowRequest/{FollowRequest}', [FollowrequestController::class,'Delete']);
+        Route::post('FollowRequest/{FollowRequest}', [FollowrequestController::class,'create'])->middleware('verified');
+        // Followers
+        Route::get('Follower',[FollowerController::class,'index']);
+        Route::delete('Follower/{follower}', [FollowerController::class,'Delete']);
+        // Followings
+        Route::get('Following',[FollowingController::class,'index']);
+        Route::delete('Following/{following}', [FollowingController::class,'Delete']);
 
 
 
+    });
+    // Auth
+    Route::post('Signup',[AuthController::class,'Signup']);
+    Route::post('Login', [AuthController::class,'Login']);
+    Route::post('Logout', [AuthController::class,'Logout'])->middleware('auth:api');
+    //User Profile Middleware
+    Route::group(['middleware'=>'auth:api','prefix'=>'Profile'],function()
+    {
+        Route::get('myprofile',[UserController::class,'index']);
+        Route::put('editprofile',[UserController::class,'Update']);
+        Route::delete('deleteaccount', [UserController::class,'Delete']);
+        Route::put('Changepassword', [UserController::class,'ChangePassword']);
+    });
+
+    //email verification
+    // Verify email
+    Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke']);
+    Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke']);
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store']);
 });
-// Auth
-Route::post('Signup',[AuthController::class,'Signup']);
-Route::post('Login', [AuthController::class,'Login']);
-Route::post('Logout', [AuthController::class,'Logout'])->middleware('auth:api');
-//User Profile Middleware
-Route::group(['middleware'=>'auth:api','prefix'=>'Profile'],function()
-{
-    Route::get('myprofile',[UserController::class,'index']);
-    Route::put('editprofile',[UserController::class,'Update']);
-    Route::delete('deleteaccount', [UserController::class,'Delete']);
-    Route::put('Changepassword', [UserController::class,'ChangePassword']);
-});
 
-//email verification
-// Verify email
-Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke']);
-Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke']);
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store']);
-
-// Priacy of User,block user, Forget Password ,Email verification
+// block user, Forget Password ,Email verification ,Phone verification
 //Extra: Gmail Api, Filter,chat ,story Search as feature
 // Exception handler , more middle ware for json response, Base64 for images
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
